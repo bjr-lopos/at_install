b9d14a437fd67bece717926606c541ce78701d1a
Bart Jooris <bart.jooris@gmail.com>
2020-04-15 18:17:25 +0200

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+loposCoreBin=/usr/local/bin/loposcore
+ROOTPASS=LoPoS
+USERLOGIN=lopos_test
+#USERPASS=nouser
+USERPASS=lopos_test
+TARGET_DB=lopos_test
+
+installDB() {
+createUserTemp=/tmp/loposdb_cu.sql
+
+PARAM="AU IS FD T1"
+if [ ! -z "$1" ]; then
+    if [ "$1" = "-v" ]; then
+        shift
+    fi
+    PARAM="$*"
+fi
+
+
+if echo "$PARAM" | grep 'AU'; then
+    echo "add user, database and access to it"
+    ./initdb.sh $TARGET_DB $USERLOGIN $USERPASS > $createUserTemp
+    mysql -uroot -p$ROOTPASS < $createUserTemp
+fi
+
+if echo "$PARAM" | grep 'IS'; then
+    echo "install schema"
+    mysql -uroot -p$ROOTPASS $TARGET_DB < lopos_schema.sql
+fi
+
+if echo "$PARAM" | grep 'FD'; then
+    echo "flood the device table"
+    mysql -uroot -p$ROOTPASS $TARGET_DB < device.sql
+fi
+
+if echo "$PARAM" | grep 'T1'; then
+    echo "Verify, changes to the schema can be store by using CS"
+    mysql -uroot -p$ROOTPASS $TARGET_DB
+fi
+}
+
+
+createService() {
+LOPOSCORE_SERVICE=/tmp/loposcore.service
+cat << EOT > $LOPOSCORE_SERVICE
+[Unit]
+Description=loposcore service
+#Requires=mysql.service
+#After=mysql.service
+
+[Service]
+Environment=SERIAL_PORT=/dev/ttyUSB0
+ExecStart=/usr/local/bin/loposcore dev=\${SERIAL_PORT} db_user=$USERLOGIN db_pass=$USERPASS db_name=$TARGET_DB dumpFrames2Log=no dumpHdlInfo2Log=000000000100010000 dumpGenInfo2Log=0101
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=lcs
+
+[Install]
+#WantedBy=multi-user.target mysql.service
+WantedBy=multi-user.target
+EOT
+
+sudo cp $LOPOSCORE_SERVICE /etc/systemd/system/
+
+}
+
+createService
+exit
+
+#`service loposcore log | grep unrecognized` 
+#if 
+if [ ! -e $loposCoreBin ]; then
+    sudo apt-get update
+    export DEBIAN_FRONTEND=noninteractive
+	sudo -E apt-get -q -y install mysql-server
+    sudo mysqladmin -u root password $ROOTPASS
+    installDB
+    sudo sed 's/\(^.*bind-address.*$\)/#\1/' -i /etc/mysql/mysql.conf.d/mysqld.cnf
+    sudo service mysql restart
+    createService
+else 
+    service loposcore stop
+    systemctl disable loposcore.service
+fi
+
+sudo cp loposcore $loposCoreBin
+systemctl enable loposcore.service
+service loposcore start
+
+
---
+++ initdb.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+DBNAME=lopos
+if [ ! -z "$1" ]; then
+    DBNAME=$1
+fi
+USER=lopos
+if [ ! -z "$2" ]; then
+    USER=$2
+fi
+PASS=lopos
+#pass equal to nouser will no create a new user!
+if [ ! -z "$3" ]; then
+    PASS=$3
+fi
+
+printf "create database IF NOT EXISTS %s;\n" $DBNAME
+printf "use mysql;\n"
+if [ ! "$PASS" = "nouser" ]; then  
+    printf "CREATE USER '%s'@'localhost' IDENTIFIED BY '%s';\n" $USER $PASS
+    printf "CREATE USER '%s'@'%%' IDENTIFIED BY '%s';\n" $USER $PASS
+fi
+printf "insert into db (host,Db,User) values(\"localhost\",\"%s\",\"%s\");\n" $DBNAME $USER
+printf "insert into db (host,Db,User) values(\"%%\",\"%s\",\"%s\");\n\n" $DBNAME $USER
+
+printf "update db set 
+Select_priv = \"Y\", 
+Insert_priv = \"Y\", 
+Update_priv = \"N\", 
+Delete_priv = \"N\", 
+Index_priv  = \"Y\", 
+Execute_priv  = \"Y\", 
+Create_view_priv = \"Y\", 
+Show_view_priv = \"Y\" 
+where user = \"%s\" AND host=\"localhost\";\n\n"  $USER
+
+printf "update db set 
+Select_priv = \"Y\", 
+Insert_priv = \"Y\", 
+Update_priv = \"N\", 
+Delete_priv = \"N\", 
+Index_priv  = \"Y\", 
+Execute_priv  = \"Y\", 
+Create_view_priv = \"Y\", 
+Show_view_priv = \"Y\" 
+where user = \"%s\" AND host=\"%%\";\n\n" $USER
+
+
+printf "flush privileges;\n"
+printf "use %s;\n"  $DBNAME
+printf "#please execute:\n#\tmysql -u root -p < this_output.sql\n"

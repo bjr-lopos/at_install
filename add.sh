987fbe7cb483e88484e981311a242adcd3c65c01
Bart Jooris <bart.jooris@gmail.com>
2020-07-25 19:19:30 +0200

    minor changes to add

--- add.sh
+++ add.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 lclDir=`dirname $0`
 source $lclDir/local_cfg
-id=$(($1+4096))
-devsql=`printf "insert into device values (%d, \"tag%d\", 3, now());" $id $id`
-plansql=`printf "insert into plan values (%d, 12, 320);" $id`
+offset=4096
+profile=tag
+if [ "$1" = "tag" ]; then
+    offset=4096
+    profile=tag
+    shift
+elif [ "$1" = "anchor" ]; then
+    offset=40960
+    profile=anchor
+    shift
+elif [ "$1" = "sink" ]; then
+    offset=65520
+    profile=sink
+    shift
+else
+    echo "Warning default profile is $profile"
+fi
+id=$(($1+$offset))
+interval=320
+if [ ! -z "$2" ]; then
+    interval=$2
+fi
+devsql=`printf "insert into device values (%d, \"$profile%d\", 3, now());" $id $id`
+plansql=`printf "insert into plan values (%d, 12, $interval);" $id`
 echo $devsql $plansql
 echo mysql -u$USERLOGIN -p$USERPASS $TARGET_DB -e "$devsql"
 echo mysql -u$USERLOGIN -p$USERPASS $TARGET_DB -e "$plansql"

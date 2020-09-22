cb34a4ea34248d3f102c5fada844ea74af2dabe0
bjooris <bart.jooris@lopos.be>
2020-09-22 13:48:38 +0200

    minor

--- checkUWBtxPwr.sql
+++ checkUWBtxPwr.sql
@@ -1,20 +1,14 @@
-
-
-
-
-
-
 SELECT
             d.addr,
-	    s.uwbTxPwr as isTx,
-            d.uwbTxPower as setTx
-        FROM
+	    ((6 - (s.uwbTxPwr div 32)) * 3) + round((s.uwbTxPwr & 0x01F)/2) as isUwbTxPwr,
+            d.uwbTxPower as setUwbTxPwr
+FROM
             device as d,
 	    stat as s,
             (select addr, max(updated) as last_update from stat group by addr) as ref
-        where
+WHERE
             s.addr = d.addr and
             s.addr = ref.addr and
 	    s.updated = ref.last_update and
             d.uwbTxPower > 0 and
-            d.uwbTxPower <> s.uwbTxPwr
+            d.uwbTxPower <> ((6 - (s.uwbTxPwr div 32)) * 3) + round((s.uwbTxPwr & 0x01F)/2)

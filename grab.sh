a6718dacf646af66d304f0bd4e73e0489caa01cc
bjooris <bart.jooris@lopos.be>
2021-07-07 12:08:04 +0200

    grabAccel!

--- grab.sh
+++ grab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#GRANT FILE ON *.* TO 'lopos'@'localhost';
+GRANT FILE ON *.* TO 'lopos'@'localhost';
 mysql -uilvo -pilv0 ilvo -e "select * from position where addr=0x1003 and UNIX_TIMESTAMP(updated) >= UNIX_TIMESTAMP(\"2020-05-15 15:02:19.448\");"
 mysql -uilvo -pilv0 ilvo -e "select * from position where addr=0x1003 and UNIX_TIMESTAMP(updated) >= UNIX_TIMESTAMP(\"2020-05-15 15:02:19.448\") INTO OUTFILE '/tmp/exp_15_05_2020.csv' FIELDS ENCLOSED BY '\"'  TERMINATED BY ';'  ESCAPED BY '\"'  LINES TERMINATED BY '\r\n';"
 

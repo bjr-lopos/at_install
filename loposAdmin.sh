06cfd1ea1c8837b6a648c46684eb74eeea49ecb4
bjooris <bart.jooris@lopos.be>
2021-11-04 08:52:21 +0100

    after upgrade to 20.04

--- loposAdmin.sh
+++ loposAdmin.sh
@@ -2,6 +2,6 @@
 #visudo mike ALL=(ALL) NOPASSWD: /home/lopos/install/loposAdmin.sh
 #whoami
 cd /home/lopos/install
-export PYTHONPATH="/usr/lib/python36.zip:/usr/lib/python3.6:/usr/lib/python3.6/lib-dynload:/home/lopos/.local/lib/python3.6/site-packages:/usr/local/lib/python3.6/dist-packages:/usr/lib/python3/dist-packages"
-#echo $PYTHONPATH
+export PYTHONPATH=`python -c "import site, os; print(os.path.join(site.USER_BASE, 'lib', 'python', 'site-packages'))"`
+echo $PYTHONPATH
 python3 loposAdmin.py $*
